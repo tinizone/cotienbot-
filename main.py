61c2d48bcf7b183f68837fb37fca31b13559e1c2
tinizone <thaiquangvinh@hotmail.com.vn>
2025-05-07 12:16:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ def health():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
