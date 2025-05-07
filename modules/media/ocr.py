11b38c64e676e5c3114981caafc33d4ef21068c0
tinizone <thaiquangvinh@hotmail.com.vn>
2025-05-07 11:12:41 +0700

    Delete modules directory

--- modules/media/ocr.py
+++
@@ -1,5 +0,0 @@
-async def process_photo(photo_data: bytes) -> str:
-    from google.generativeai import GenerativeModel
-    model = GenerativeModel(get_latest_model())
-    response = model.generate_content(["Describe this image", photo_data])
-    return response.text
